7c9ed73214eff9c2e0c5dab164dd66a1765d58dc
hw <brightrain453@gmail.com>
2026-01-21 22:11:42 +0900

    at home

---
+++ SWEA-5186.py
@@ -0,0 +1,20 @@
+# SWEA 문제 5186번
+# 주어진 실수를 이진법으로 변환하는 문제였습니다. for문과 if문을 활용해 잘 풀어내려했지만 생각한대로 잘 되지만은 않았습니다.
+# 사고를 조금 더 하고 코드를 다듬으면 해결할 수 있다고 생각합니다. 추후 해결하여 결과 커밋하겠습니다.
+
+num_case = int(input())
+
+N = float(input())
+
+Binary = []
+
+for e in range(0,15):
+    if N == 0:
+        break
+    elif 2**e > N:
+        N -= 2**-(e-1)
+        Binary.append(0)
+    else:
+        Binary.append(1)
+
+print(Binary, sep=' ')
