ac5b5bcd6ed0814319dccf50edbfca624aee40e8
Shawn <jsucheol77@naver.com>
2026-01-24 23:34:12 +0900

    learned: swea5186 reivew

--- jsc/algorithm-study/Week-01/swea-5186.py
+++ jsc/algorithm-study/Week-01/swea-5186.py
@@ -1,18 +1,210 @@
+
+# ---
+# 4번째(최신)
+# ---
+
 T = int(input())
 for _ in range(1,T+1):
-    result = []
-    num = int(input())
-    for i in # num = 0 까지:
-        if num - 2**(i) < 0:
-            result.append('1')
-        else:
-            result.append('0')
-            num -= 2**(i)
+    result = ''
+    n = float(input())
+    i = 1
+    while n > 0:
+        if i == 13:
+            print('overflow')
+            break
+        if n >= 2**(-i):
+            result += '1'
+            n -= 2**(-i)
+            i += 1
+            print(result)
+        else: 
+            result += '0'
+            i += 1
+            continue
+            
 
-print(f"#{T} {*{result}}")
+print(result)
+
+
+
+
+# ---
+# 아카이브
+
+
+# ---
+# 1번째
+# ---
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = []
+#     num = int(input())
+#     for i in # num = 0 까지:
+#         if num - 2**(i) < 0:
+#             result.append('1')
+#         else:
+#             result.append('0')
+#             num -= 2**(i)
+
+# print(f"#{T} {*{result}}")
 
 
 ## 내가 모르는 것
 # for문을 언제 끝나는지 지정
 # 리스트의 값들을 대괄호없이
 
+
+
+# ---
+# 2번째
+# ---
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = []
+#     n = float(input())
+#     i = 1
+#     while n >= 0:
+#         if n == 0:
+#             print(*result)
+#             break
+#         if i == 13:
+#             print('overflow')
+#             break
+#         result.append(1)
+#         n -= 2**(-i)
+#         i += 1
+#         print(*result)
+#     else:
+#         result.append(0)
+#         n -= 2**(-i)
+#         print(*result)
+#         i += 1
+
+# print(*result)
+
+
+## 내가 모르는 것
+# for문을 언제 끝나는지 지정
+# 리스트의 값들을 대괄호없이
+
+# ---
+# 3번째
+# ---
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = ''
+#     n = float(input())
+#     i = 1
+#     while n > 0:
+#         if i == 13:
+#             print('overflow')
+#             break
+#         if n >= 2**(-i):
+#             result += '1'
+#             n -= 2**(-i)
+#             i += 1
+#             print(result)
+#         else: 
+#             result += '0'
+#             a = n
+#             n -= 2**(-i)
+#             i += 1
+#             print(result)
+#             if n <0:
+#                 n -= 2**(-i)
+#                 result += '1'
+#             else:
+#                 result += '0'
+# print(result)
+
+
+
+
+
+# ---
+# 1번째
+# ---
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = []
+#     n = float(input())
+#     i = 1
+#     while n >= 0:
+#         if n == 0:
+#             print(*result)
+#             break
+#         if i == 13:
+#             print('overflow')
+#             break
+#         result.append(1)
+#         n -= 2**(-i)
+#         i += 1
+#         print(*result)
+#     else:
+#         result.append(0)
+#         n -= 2**(-i)
+#         print(*result)
+#         i += 1
+
+# print(*result)
+
+
+## 내가 모르는 것
+# for문을 언제 끝나는지 지정
+# 리스트의 값들을 대괄호없이
+
+# ---
+# 2번째
+# ---
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = ''
+#     n = float(input())
+#     i = 1
+#     while n > 0:
+#         if i == 13:
+#             print('overflow')
+#             break
+#         if n >= 2**(-i):
+#             result += '1'
+#             n -= 2**(-i)
+#             i += 1
+#             print(result)
+#         else: 
+#             result += '0'
+#             a = n
+#             n -= 2**(-i)
+#             i += 1
+#             print(result)
+#             if n <0:
+#                 n -= 2**(-i)
+#                 result += '1'
+#             else:
+#                 result += '0'
+# print(result)
+
+# T = int(input())
+# for _ in range(1,T+1):
+#     result = ''
+#     n = float(input())
+#     i = 1
+#     while n > 0:
+#         if i == 13:
+#             print('overflow')
+#             break
+#         if n >= 2**(-i):
+#             result += '1'
+#             n -= 2**(-i)
+#             i += 1
+#         else: 
+#             result += '0'
+#             i += 1
+#             continue
+            
+
+# print(result)
