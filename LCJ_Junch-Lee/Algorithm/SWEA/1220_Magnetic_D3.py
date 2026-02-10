75bda6796ada106f85cb6b6d03662bafca2b3979
이창준 <lcj00803@naver.com>
2026-02-10 11:59:43 +0900

    Solved : 2805 농작물 수확 추가

--- LCJ_Junch-Lee/Algorithm/SWEA/1220_Magnetic_D3.py
+++ LCJ_Junch-Lee/Algorithm/SWEA/1220_Magnetic_D3.py
@@ -1,25 +1,6 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-# 문제 풀이 포인트
-# 1. 교착 상태 : 1-N극, 2-S극이 맞붙은 상태
-# 카운팅 기준 : 1-2가 교차되어 나타날 때 교착 +1 
-
-# 2. 순회 기준? 
-# 열 -> 행으로 시도
-
-# 3. 탐색 과정?
-# (1) 0은 건너뛰기
-# (2) if arr[r][c] != 0 -> 세부 탐색
-# (2)-1. 1을 만났을 때 
-# --> range(r+1 ~ N): 아래에 2가 있는 경우에 deadlock += 1
-
-# 카운팅을 해준다면? 
-# if 1 만나면 north = True
-# -> 2 만나고, north = True : deadlock + 1 , North = False
-
-# else (2)-2. 2를 먼저 만나면 -> skip
-
 def find_deadlock(arr, N):
 
     deadlock = 0 # 교착 상태
@@ -48,3 +29,22 @@ for tc in range(1,11):
     result = find_deadlock(arr, N)
 
     print(f"#{tc} {result}") 
+
+
+'''
+# 문제 풀이 포인트
+# 1. 교착 상태 : 1-N극, 2-S극이 맞붙은 상태
+# 카운팅 기준 : 1-2가 교차되어 나타날 때 교착 +1 
+
+# 2. 순회 기준? 
+# 열 -> 행으로 시도
+
+# 3. 탐색 과정?
+# (1) 0은 건너뛰기
+# (2) if arr[r][c] != 0 -> 세부 탐색
+#    ㄱ. 1을 만났을 때 : North = True로 정의
+
+#    ㄴ. North -> True일 때, 2를 만나면 deadlock +=1
+
+#    ㄷ. 2를 먼저 만나면? -> skip
+'''
